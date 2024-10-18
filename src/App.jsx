6a3bdda0f5c62033aceb8e0f8d6ded40ec629ab7
B200896 <kashishkaramchandani3@gmail.com>
2024-10-18 15:03:38 +0530

    final updated code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@ import Header from "./Components/Header.jsx";
 import NavBar from "./Components/NavBar.jsx"
 import Imagegrid from "./Components/Imagegrid.jsx";
 function App() {
+  const cards = [
+    {
+      src: '/Smiling Woman On Phone 1 (1).png',
+      title: 'Guides and tutorials',
+    },
+    {
+      src: '/Smiling Woman On Phone 1 (1).png',
+      title: 'Frequently Asked Questions',
+    },
+    {
+      src: '/Smiling Woman On Phone 1 (1).png',
+      title: 'Privacy and security',
+    },
+    {
+      src: '/Smiling Woman On Phone 1 (1).png',
+      title: 'Contact us',
+    },
+  ];
   return (
     <>
       <NavBar />
       <Header />
-      <Imagegrid/>
+      <Imagegrid cardd={cards}/>
     </>
   );
 }
